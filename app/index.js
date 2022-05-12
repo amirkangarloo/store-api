aaebb2d9644aae6e4b840db87ef57460e4fecddb
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-12 11:44:14 +0430

    Implement middleware and .env and start App

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+
+
+
+module.exports = (port) => {
+    app.listen(port, () => {
+        console.log(`App listen on port ${port} ...`);
+    })
+}
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 'use strict'
+
+require('dotenv').config()
+const startApp = require('./app')
+
+startApp(process.env.APP_PORT)
