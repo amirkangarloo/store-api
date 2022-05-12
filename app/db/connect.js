78a0ff93ed9903e3ab9b78db4e1f1914706accc7
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-12 12:28:45 +0430

    Develop app (index.js) and implement connect to DB

--- app/db/connect.js
+++ app/db/connect.js
@@ -1,1 +1,9 @@
 'use strict'
+
+const mongoose = require('mongoose')
+
+const connectDB = (url, dbName) => {
+    mongoose.connect(`${url}/${dbName}`)
+}
+
+module.exports = connectDB
--- app/index.js
+++ app/index.js
@@ -3,16 +3,39 @@
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
 // middleware
 app.use(express.json())
 
+// roote
+app.get('/', (req, res) => {
+    res.status(200).send(
+        '<h1> Store API </h1> <a href="/api/v1/products"> products route </a>'
+    )
+})
+
+// products route
 
 
-module.exports = (port) => {
-    app.listen(port, () => {
-        console.log(`App listen on port ${port} ...`);
-    })
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+module.exports = async (port) => {
+    try {
+        // connectDB
+        const {
+            MONGO_URL,
+            MONGO_DB_NAME
+        } = process.env
+        await connectDB(MONGO_URL, MONGO_DB_NAME)
+        app.listen(port, () => {
+            console.log(`App listen on port ${port} ...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
