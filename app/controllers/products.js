98f7d6744d9c24b7fc106d5f6303d03699875af5
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-14 13:58:01 +0430

    Use product model on controller

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res, next) => {
     throw new Error('express testing error')
     res.status(200).send({
@@ -8,8 +10,25 @@ const getAllProductsStatic = async (req, res, next) => {
 }
 
 const getAllProducts = async (req, res, next) => {
+
+    const {featured, company, name} = req.query
+    const queryObj = {}
+
+    if (featured) {
+        queryObj.featured = featured === "true" ? true : false
+    } 
+    if (company) {
+        queryObj.company = company
+    }
+    if (name) {
+        queryObj.name = name
+    }
+
+    const products = await Product.find(queryObj)
+
     res.status(200).send({
-        msg: "products route"
+        "Number of producs": products.length,
+        products
     })
 }
 
